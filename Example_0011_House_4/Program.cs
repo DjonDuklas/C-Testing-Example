b86c86f5579fea225894aed50ee9e5fc4829fa5c
««NikolayKuleshov <89391@mail.ru>
2022-10-01 20:23:26 +0900

    Сделал 4 домашнее задание

---
+++ Example_0011_House_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+
+while(count <= number)
+{
+    if (count % 2 == 0)
+        {
+        Console.Write($"{count}, ");
+        }
+
+    count = count + 1;
+
+}
+Console.Write("\n");
+Console.WriteLine("Показали все чётные числа от 1 до Введенного числа");
