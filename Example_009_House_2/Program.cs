22fe96393fb93ae3896d998ed8f5c94363198b8e
««NikolayKuleshov <89391@mail.ru>
2022-10-01 19:37:28 +0900

    Сделал 2 домашнее задание

---
+++ Example_009_House_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int NumberOne = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int NumberTwo = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int NumberThree = int.Parse(Console.ReadLine()!);
+
+int max = NumberOne;
+
+if(NumberTwo > max ) max = NumberTwo;
+if(NumberThree > max) max = NumberThree;
+
+Console.WriteLine($"Большим числом является {max}");
