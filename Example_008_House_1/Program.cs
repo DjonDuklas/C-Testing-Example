e9e05aa5b3a111da35323c1a9f32848d77be7338
««NikolayKuleshov <89391@mail.ru>
2022-10-01 19:32:09 +0900

    Сделал 1 домашнее задание

---
+++ Example_008_House_1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int max = 0;
+int min = 0;
+
+Console.Write("Введите первое число: ");
+int NumberOne = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int NumberTwo = int.Parse(Console.ReadLine()!);
+
+if (NumberOne > NumberTwo)
+{
+    max = NumberOne;
+    min = NumberTwo;
+} 
+if (NumberOne < NumberTwo)
+{
+    max = NumberTwo;
+    min = NumberOne;
+} 
+
+Console.WriteLine($"Большим числом является {max} , а меньшим {min} ");
