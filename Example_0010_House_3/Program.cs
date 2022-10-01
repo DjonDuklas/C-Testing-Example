df6813251a7fd245dbabca4c08deccd30491a0a7
««NikolayKuleshov <89391@mail.ru>
2022-10-01 19:46:38 +0900

    Сделал 3 домашнее задание

---
+++ Example_0010_House_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+    {
+    Console.Write("Введенное число является четным");
+    Console.Read();
+    }
+else
+    {
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+    }
