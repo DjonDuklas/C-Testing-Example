5d5a6c7571c0b1cfb89864cc14ccee58b768db24
««NikolayKuleshov <89391@mail.ru>
2022-10-06 23:27:20 +0900

    Сделал третью  домашнюю задачу

---
+++ Example_0016_ArraysAndFunction_House_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+
+    if (number >= 1 && number <= 7)
+    {
+        if (number == 6 | number == 7) Console.WriteLine("Сегодня выходной");
+        else Console.WriteLine("Сегодня рабочий день");
+    }
+    else Console.WriteLine("Не верное число");
