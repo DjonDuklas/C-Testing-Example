b784e1786b57c4a347ed1c9c4f3b53e12e48d376
««NikolayKuleshov <89391@mail.ru>
2022-10-06 23:21:48 +0900

    Сделал вторую  домашнюю задачу

---
+++ Example_0016_ArraysAndFunction_House_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+
+if (number / 100 > 1)
+    {
+    string a3 = number.ToString();
+    Console.WriteLine ($"Третья цифра числа {a3[2]}");
+    }
+
+else
+    {
+    Console.WriteLine ("Третьей цифры нет");
+    }
