0e72235e478dd0ae98f2a057d5766c42a7332d2c
««NikolayKuleshov <89391@mail.ru>
2022-10-13 14:17:18 +0900

    Сделал третье  домашнее задание

---
+++ Example_0023_House_13.10.2022_Three/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] RandomArr()
+{
+    int[] array = new int[8];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 9);
+    }
+    return array;
+} 
+
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        Console.Write($"{mass[i]}");
+        if (i != mass.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+int[] N = RandomArr();
+PrintArray(N);
