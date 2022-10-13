18a3e24c689f0a02418198e58902a75a4090f239
««NikolayKuleshov <89391@mail.ru>
2022-10-13 13:49:26 +0900

    Сделал второе  домашнее задание

---
+++ Example_0022_House_13.10.2022_Two/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSum (int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int i = number % 10;
+        number = number / 10;
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetSum(number));
