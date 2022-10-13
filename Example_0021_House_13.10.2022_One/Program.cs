5923436c039d93e18fe4944b422ce4498ba1c0b4
««NikolayKuleshov <89391@mail.ru>
2022-10-13 13:28:22 +0900

    Сделал первое домашнее задание

---
+++ Example_0021_House_13.10.2022_One/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+void Nature (int A, int B)
+{
+    int result = 1;
+    for(int n = 1; n <= B; n++)
+    {
+        result = result * A; 
+    }
+    Console.WriteLine(result);
+}
+
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Nature(A, B);
